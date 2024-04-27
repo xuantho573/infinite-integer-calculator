10be467bb1fd766d181acb6ac8d8c1dd6da9c0e3
Nguyễn Xuân Thọ <xuantho573@gmail.com>
2024-04-27 17:21:05 +0700

    chore: config vite build

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+
+import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "index",
+      fileName: "index",
+      formats: ["cjs", "es"]
+    }
+  },
+  test: {
+    globals: true
+  },
+  plugins: [dts()]
+});
