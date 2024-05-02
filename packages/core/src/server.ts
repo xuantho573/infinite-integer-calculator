33e790a345783bb61539118a9b9b1edf9b393e7e
Nguyễn Xuân Thọ <xuantho573@gmail.com>
2024-05-02 14:20:42 +0700

    chore: implement a simple http server

---
+++ packages/core/src/server.ts
@@ -0,0 +1,26 @@
+import { calc } from "./parser";
+
+import cors from "cors";
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors<Request>());
+app.use(express.json());
+
+app.get("/", (_: Request, res: Response) => {
+  res.send("<h2>Server is up and running</h2>");
+});
+
+app.post("/", (req: Request, res: Response) => {
+  try {
+    res.status(200).json({ val: calc(req.body.expr).toString() });
+  } catch (error: any) {
+    res.status(400).json({ error: "Invalid expression" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
